bf535a23f8811ef65caffe654dd0da90ce0a7d12
xk404error <528790xk>
2019-11-25 23:35:04 +0800

    链表操作

--- src/main/java/Test21.java
+++ src/main/java/Test21.java
@@ -1,2 +1,41 @@
+import model.ListNode;
+
+//合并两个有序链表
 public class Test21 {
+    //空间复杂度m+n，时间复杂度m+n
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null){
+            return l2;
+        }
+        if(l2==null){
+            return l1;
+        }
+        if(l1.val<l2.val){
+            l1.next=mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next=mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+
+    }
+
+
+    //空间复杂度1，时间复杂度m+n
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2){
+        ListNode pre=new ListNode(-1);
+        ListNode res=pre;
+        while (l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                pre.next=l1;
+                l1=l1.next;
+            }else{
+                pre.next=l2;
+                l2=l2.next;
+            }
+            pre=pre.next;
+        }
+        pre.next=l1==null?l2:l1;
+        return res.next;
+    }
 }
--- src/main/java/Test876.java
+++ src/main/java/Test876.java
@@ -1,5 +1,29 @@
+import model.ListNode;
 
-
+//链表的中间节点
 public class Test876 {
+    //用数组存储所有node，找到数组的中间位置
+    public ListNode middleNode(ListNode head) {
+        ListNode[] arr=new ListNode[100];
+        int t=0;
+        while (head!=null){
+            arr[t++]=head;
+            head=head.next;
+        }
 
+        return  arr[t/2];
+    }
+    //快慢两次遍历，快的每次两步，慢的一步。快的到尾时，慢的正好在中间
+    public ListNode middleNode1(ListNode head){
+        ListNode fast=new ListNode(2);
+        ListNode slow=new ListNode(1);
+        fast.next=head;
+        slow.next=head;
+        while (fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow;
+
+    }
 }
